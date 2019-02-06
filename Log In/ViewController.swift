f68d9995e7fbc102e62bcdaa1fa01182f85a8838
TIEGO OUEDRAOGO <tiegi.ouedraogo@stu.bmcc.cuny.edu>
2019-02-05 22:31:54 -0500

    login found
    this is the that always crushes

--- Log In/ViewController.swift
+++ Log In/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var login: UIButton!
+    @IBOutlet weak var forgotUserName: UIButton!
+    @IBOutlet weak var forgotPassWord: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender:
+        Any?) {
+        segue.destination.navigationItem.title = textField.text
+        
+        guard let sender = sender as? UIButton else {
+            return;
+        }
+            if sender == forgotPassWord{
+                segue.destination.navigationItem.title = "ForgotPassWord"
+            }else{
+                if sender == forgotUserName{
+                    segue.destination.navigationItem.title = "forgotUserName"
+                }else{
+                    segue.destination.navigationItem.title = textField.text
+                }
+            }
+        }
+    
+  
+    @IBAction func logInButton(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "logIn", sender: login)
+    }
+    
+    @IBAction func userNameButton(_ sender: UIButton) {
+        
+         performSegue(withIdentifier: "UserName", sender: forgotUserName)
+    }
+    
+    @IBAction func passWordButton(_ sender: UIButton) {
+        
+         performSegue(withIdentifier: "PassWord", sender: forgotPassWord)
+    }
+    
 }
 
